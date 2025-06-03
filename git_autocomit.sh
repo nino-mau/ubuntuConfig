394c909115f1c8a5276267e09d138f7b137c8685
nino.mauu <nino.mauuary@gmail.com>
2025-06-03 22:06:53 +0200

    Automated commit on mar. 03 juin 2025 22:06:53 CEST

---
+++ git_autocomit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Navigate to your repository
+cd ~/Documents/dev/backups/ubuntuConfig/. || exit
+
+# Check for changes
+if git diff --exit-code --quiet && git diff --cached --exit-code --quiet; then
+  echo "No changes to commit."
+  exit
+fi
+
+# Configure Git user
+git config user.email "nino.mauuary@gmail.com"
+git config user.name "nino.mauu"
+
+# Stage and commit changes
+git add .
+git commit -m "Automated commit on $(date)"
+
+# Push changes
+git push origin main
