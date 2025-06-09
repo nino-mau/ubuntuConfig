7a0d066f2b15af214500df0f25cb5f28b19510e0
nino.mauu <nino.mauuary@gmail.com>
2025-06-09 22:16:13 +0200

    Update config, add a script to update config easily

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,193 @@
-/home/nino/.config/fish/config.fish
+# Set oh my posh theme, change the omp.json part to change theme
+
+oh-my-posh init fish --config $HOME/.poshthemes/star.omp.json | source
+
+##
+## PATH
+##
+
+alias cdhtml="cd /opt/lampp/htdocs" # Open Xampp site folder
+
+alias cdconf="cd ~/.config" # Open .config folder
+
+alias cddev="cd ~/Documents/dev" # Open dev folder
+
+alias cdgv="cd ~/Documents/dev/projects/Gameverse" # Open gameverse project
+
+alias cdpy="cd ~/Documents/dev/python" # go to python dir
+
+alias cdproj="cd ~/Documents/dev/projects" # go to project folder
+
+alias cddwl="cd ~/Downloads" # go to download folder
+
+alias cdTmp="cd ~/Documents/tmp" # go to tmp folder
+
+##
+## PROJECTS
+##
+
+alias nvgv="nvim ~/Documents/dev/projects/Gameverse" # Open gamverse projects in nvim
+
+alias lvgv="lvim ~/Documents/dev/projects/Gameverse" # Open gamverse projects in lazyvim
+
+##
+## FISH
+##
+
+alias fconfig="kate $HOME/.config/fish/config.fish" # Open fish config file in kate
+
+alias rconfig="source ~/.config/fish/config.fish" # Reload fish config file
+
+##
+## VPS
+##
+
+alias cvps="TERM=xterm ssh nino@164.92.184.136" # Connect to digital ocean droplet
+
+alias crootvps="TERM=xterm ssh root@164.92.184.136" # Connect to digital ocean droplet as root
+
+##
+## XAMPP/LAMP
+##
+
+alias cxamp="sudo /opt/lampp/manager-linux-x64.run" # Start xampp GUI client
+
+alias xampphpini="sudo nano /opt/lampp/etc/php.ini" # Open the php.ini file for xampp
+
+alias vhconf="sudo nano /opt/lampp/etc/extra/httpd-vhosts.conf" # Open Xampp vhost configuration file
+
+##
+## PYTHON
+##
+
+alias rvenv="source venv/bin/activate.fish" # Start python venv
+
+alias dvenv="deactivate" # deactivate python venv
+
+# Start and run python env
+function svenv
+    python3 -m venv venv
+    source venv/bin/activate.fish
+end
+
+##
+## NEOVIM
+##
+
+alias lvim="NVIM_APPNAME=lazyvim /usr/local/bin/nvim"
+
+alias astrovim="NVIM_APPNAME=astrovim nvim" # Open nvim using the astrovim config version
+
+# alias lvim="NVIM_APPNAME=lazyvim nvim" # Open nvim using the lazy vim config version
+
+alias lvconf="lvim ~/.config/lazyvim" # Open default lazyvim config in lazyvim
+
+alias nvconf="nvim ~/.config/nvim" # Open default nvim config in nvim
+
+##
+## NGINX
+##
+
+alias rnginx="sudo systemctl restart nginx" # Reload nginx 
+
+##
+## DOCKER
+##
+
+alias rdock="docker compose up -d" # start docker 
+ 
+alias builddock="docker compose up -d --build" # rebuild docker
+
+alias sdock="docker compose down" # stop docker
+
+alias srdock="docker compose down -v" # remove docker contains
+
+alias bashdock="docker compose exec app bash" # access console inside container
+
+alias migratedock="docker-compose exec app php artisan migrate" # run laravel db migration
+
+alias logdock="docker compose exec app tail /var/www/storage/logs/laravel.log" # show log for app in docker
+
+##
+## LS
+##
+
+alias ls="lsd -r" # Execute custom ls
+
+alias lsp="lsd --blocks=user,group,permission,name" # Execute custom ls
+
+alias lsl="lsd -latr --blocks=date,user,group,permission,name" # Execute custom ls
+
+##
+## GENERAL
+##
+
+alias fkitty="kate ~/.config/kitty/kitty.conf" # Open kitty terminal conf file
+
+alias fwez="kate ~/.config/wezterm/wezterm.lua" # Open wezterm terminal conf file
+
+alias phpini="sudo nano /etc/php/8.3/cli/php.ini" # Open the php.ini file used by system
+
+alias composer="php /usr/local/bin/composer/composer.phar" # Execute composer commands
+
+# Start gameverse project
+function startgv
+    # Start services
+    sudo systemctl start nginx
+    sudo systemctl start mysql
+
+    # Open server directory in VS Code
+    cd ~/Documents/GamesMatch/server
+    code .
+
+    # Open client directory in VS Code
+    cd ~/Documents/GamesMatch/client
+    code .
+
+    # Start client development server
+    npm run dev &
+
+    # Open a new WezTerm tab for the server with nodemon
+    wezterm cli spawn --cwd ~/Documents/GamesMatch/server -- nodemon app.js
+end
+
+##
+## UTILS
+##
+
+alias fd="fdfind"
+
+alias restart="exec $SHELL"
+
+
+# Find a directory
+function finddir
+    sudo find . -type d -name "$argv"
+end
+
+# Find a file
+function findfile
+    sudo find . -type f -name "$argv"
+end
+
+# Execute nano with root perm
+function rnano
+    sudo nano $argv
+end
+
+# Create a new dir and go in it
+function mkcd
+    if test (count $argv) -eq 1
+        sudo mkdir -p $argv[1]
+        and cd $argv[1]
+    else
+        echo "Usage: mkcd <directory>"
+    end
+end
+
+# pnpm
+set -gx PNPM_HOME "/home/nino/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
